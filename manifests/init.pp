24547a7e2bdd5d01911d0ddce06804c4da9e953d
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-24 17:04:51 +1100

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include acsc_e8_office_hardening
+class acsc_e8_office_hardening {
+}
