53d1b00e2f45d75176b122ecab1e7cc436030bb8
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-25 13:59:37 +1100

    Added local sids from cem_windows

---
+++ lib/facter/local_sids.rb
@@ -0,0 +1,16 @@
+# This fact is used to return a list of local SIDS so we can correctly
+# process CIS 19.x rules.  These rules must be set for each USER/SID
+# present inside HKEY_USER
+Facter.add(:local_sids) do
+  confine kernel: 'windows'
+  setcode do
+    sid_list = []
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = '(Get-ChildItem "REGISTRY::HKEY_USERS").name'
+    value = Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}"))
+    value.split.each do |line|
+      sid_list << line.split('\\')[1] if line.match?(%r{S\-1\-5\-.*\d$})
+    end
+    sid_list
+  end
+end
---
+++ spec/unit/facter/local_sids_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'facter'
+require 'facter/local_sids'
+
+describe :local_sids, type: :fact do
+  subject(:fact) { Facter.fact(:local_sids) }
+
+  before :each do
+    # perform any action that should be run before every test
+    Facter.clear
+  end
+
+  it 'returns a value' do
+    expect(fact.value).to eq('hello facter')
+  end
+end
