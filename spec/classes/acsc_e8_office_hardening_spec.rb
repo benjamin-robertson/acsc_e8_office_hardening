77fca3ee43992a974f6c17a8959feecf44d3f100
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-14 14:30:49 +1100

    Fixed lint

--- spec/classes/acsc_e8_office_hardening_spec.rb
+++ spec/classes/acsc_e8_office_hardening_spec.rb
@@ -4,13 +4,11 @@ require 'spec_helper'
 
 describe 'acsc_e8_office_hardening' do
   localsids = { 'local_sids' => [ 'Debian', 'ipaddress' ] }
-  #trusted_locations = { 'location1' => {'path' => 'c:\\temp'} }
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts.merge!(localsids) }
-      let(:params) { { 'trusted_locations' => { 'location1' => {'path' => 'c:\\temp', 'date' => '20/10/2020', 'description' => 'i am a desc'} } } }
-
+      let(:params) { { 'trusted_locations' => { 'location1' => { 'path' => 'c:\\temp', 'date' => '20/10/2020', 'description' => 'i am a desc' } } } }
 
       it { is_expected.to compile }
     end
