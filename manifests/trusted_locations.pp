cc0b3476e25c38000092cd26656fdcc462d89e7c
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-14 14:35:25 +1100

    added notify

--- manifests/trusted_locations.pp
+++ manifests/trusted_locations.pp
@@ -9,9 +9,10 @@ class acsc_e8_office_hardening::trusted_locations (
 ) {
   $lambda_count = 1
   $trusted_locations.each | String $name, Hash $location_data | {
-    acsc_e8_office_hardening::set_trusted_location {"location${lambda_count}":
-      * => $location_data,
-    }
+    #acsc_e8_office_hardening::set_trusted_location {"location${lambda_count}":
+    #  * => $location_data,
+    #}
     $lambda_count = $lambda_count + 1
+    notify { "the count is ${lambda_count}":}
   }
 }
