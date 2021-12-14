31791865ca60bca632b6623b974c87a868258b9b
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-14 16:31:14 +1100

    _index

--- manifests/trusted_locations.pp
+++ manifests/trusted_locations.pp
@@ -7,13 +7,14 @@
 class acsc_e8_office_hardening::trusted_locations (
   Hash $trusted_locations,
 ) {
+  # Check there are less than
   # Get hash keys as an array
   $hash_keys = keys($trusted_locations)
   notify { "hash keys is ${hash_keys}":}
   # Set trusted locations
   $hash_keys.each | Integer $index, String $name | {
     $_index = $index + 1
-    acsc_e8_office_hardening::set_trusted_location {"location${index}":
+    acsc_e8_office_hardening::set_trusted_location {"location${_index}":
       * => $trusted_locations[$name],
     }
 
