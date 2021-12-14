e9ce34816748b66db4d87d1279d8039ab6be027f
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-14 17:34:39 +1100

    Removed notifies

--- manifests/trusted_locations.pp
+++ manifests/trusted_locations.pp
@@ -10,7 +10,6 @@ class acsc_e8_office_hardening::trusted_locations (
   # Check there are less than
   # Get hash keys as an array
   $hash_keys = keys($trusted_locations)
-  notify { "hash keys is ${hash_keys}":}
   # Set trusted locations
   $hash_keys.each | Integer $index, String $name | {
     $_index = $index + 1
@@ -22,14 +21,12 @@ class acsc_e8_office_hardening::trusted_locations (
   # Delete unmanaged locations
   $hash_length = size($trusted_locations)
   $elements_to_delete = 20 - $hash_length
-  notify {"I will delete ${elements_to_delete}":}
   # Create 20 length array
   $delete_array = Array($elements_to_delete, false)
-  notify {"Array is ${delete_array}":}
 
   $delete_array.each | Integer $index | {
     $delelete_index = 20 - $index
-    notify {"Deleting location${delelete_index}":}
+    #notify {"Deleting location${delelete_index}":}
     acsc_e8_office_hardening::delete_trusted_location { "location${delelete_index}":}
   }
 
