41399cdcd6afbf422098226ba44c926bde44fd02
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-15 15:42:54 +1100

    Added error handling to fact

--- lib/facter/office_macro_last_run.rb
+++ lib/facter/office_macro_last_run.rb
@@ -4,8 +4,12 @@ Facter.add(:office_macro_last_run) do
   confine kernel: 'windows'
   setcode do
     value = ''
-    Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\Puppet') do |reg|
-      value = reg['office_macro_last_run']
+    begin
+      Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\Puppet') do |reg|
+        value = reg['office_macro_last_run']
+      end
+    rescue
+      value = 'first_run'
     end
     value
   end
