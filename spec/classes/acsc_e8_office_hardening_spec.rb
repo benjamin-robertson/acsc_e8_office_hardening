3e02ffc8c36c47eaf07ff114701d33d158dd980c
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-15 18:52:55 +1100

    Fixed issue with clearing macro settings

--- spec/classes/acsc_e8_office_hardening_spec.rb
+++ spec/classes/acsc_e8_office_hardening_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 describe 'acsc_e8_office_hardening' do
-  localsids = { 'local_sids' => [ 'Debian', 'ipaddress' ], 'office_macro_last_run' => 'first_run' }
+  localsids = { 'local_sids' => [ 'Debian', 'ipaddress' ], 'office_macro_last_run' => 'macros_from_trused_locations' }
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts.merge!(localsids) }
-      let(:params) { { 'trusted_locations' => { 'location1' => { 'path' => 'c:\\temp' } }, 'macro_setting' => 'macros_from_trused_locations' } }
+      let(:params) { { 'trusted_locations' => { 'location1' => { 'path' => 'c:\\temp' } }, 'macro_setting' => 'clear_macro_settings' } }
 
       it { is_expected.to compile }
     end
