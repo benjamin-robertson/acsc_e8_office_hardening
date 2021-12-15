248f0cccbb0ade7459dd112818862d32fb04512a
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-15 15:21:08 +1100

    .

--- lib/facter/office_macro_last_run.rb
+++ lib/facter/office_macro_last_run.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Facter.add(:office_macro_last_run) do
-  # https://puppet.com/docs/puppet/latest/fact_overview.html
+  confine kernel: 'windows'
   setcode do
-    'hello facter'
+    value = ''
+    Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\Puppet') do |reg|
+      value = reg['office_macro_last_run']
+    end
+    value
   end
 end
