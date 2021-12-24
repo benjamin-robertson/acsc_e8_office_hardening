117f46b9c964a835d681876be143c6539e331aa0
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-24 13:10:44 +1100

    Fix

--- lib/facter/office_macro_run_count.rb
+++ lib/facter/office_macro_run_count.rb
@@ -5,17 +5,17 @@ Facter.add(:office_macro_run_count) do
   setcode do
     value = 0
     #begin
+    Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\Puppet') do |reg|
+      value = reg['office_macro_run_count']
+    end
+    if value > 0
+      value += 1
       Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\Puppet') do |reg|
-        value = reg['office_macro_run_count']
+        reg.write('office_macro_run_count', Win32::Registry::REG_DWORD, value)
       end
-      if value > 0
-        value += 1
-        Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\Puppet') do |reg|
-          reg.write('office_macro_run_count', Win32::Registry::REG_DWORD, value)
-        end
-      end
+    end
     #rescue
-      value = 0
+    #  value = 0
     #end
     value
   end
